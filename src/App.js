09af7d65527e9177195b80153582de5ea5553262
Dilip <dilip_agheda@yahoo.com>
2019-05-04 06:22:38 +1000

    Setup bootstrap, initial CSS and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import bg from './data/images/cc-bg-1.jpg';
+import profileImage from './data/images/me.png';
 function App() {
   return (
     <div>App
-                  <div className="page-header-image image-path-home" data-parallax="true"></div>
-
+        <div className="page-header-image image-path-home" data-parallax="true"></div>
+        <div className="profile-page">
+        <div className="wrapper">
+          <div className="page-header">
+            <div className="page-header-image image-path-home" data-parallax="true"></div>
+            <div className="container">
+              <div className="content-center">
+                <div className="cc-profile-image"><a href="#"><img class="img-fluid" src={profileImage} alt="Image" ></img></a></div>
+                <div className="h2 title">Dilip Agheda</div>
+                <p className="category text-white">Full Stack Web Developer & Tea addict!</p>
+                <p className=" text-white">Let's build something together!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './sass/index.scss';
+import './sass/main.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
