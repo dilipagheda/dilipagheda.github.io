0ed24370acd549ba5724592419a5c39c532646f3
Dilip <dilip_agheda@yahoo.com>
2019-05-03 21:02:21 +1000

    Created the initial project structure and added some static assets.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import bg from './data/images/cc-bg-1.jpg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>App
+                  <div className="page-header-image image-path-home" data-parallax="true"></div>
+
     </div>
   );
 }
