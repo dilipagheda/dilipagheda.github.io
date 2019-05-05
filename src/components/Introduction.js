f20db3cf21247a378fe7a39b2854bf26b0ed4f51
Dilip <dilip_agheda@yahoo.com>
2019-05-06 09:22:50 +1000

    Parameterised image on homePage through json data.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import profileImage from "../data/images/me.png";
 import TextLoop from "react-text-loop";
 
 class Introduction extends Component {
     render(){
         return (
             <>
-                <div className="cc-profile-image"><a href="#"><img className="img-fluid" src={profileImage}
+                <div className="cc-profile-image"><a href="#"><img className="img-fluid" src={this.props.home.image}
                                                                    alt="Image"></img></a></div>
                 <h1>{this.props.home.name}</h1>
                 <TextLoop  className="intro-job-title" springConfig={{stiffness: 180, damping: 8}}
--- src/data/content/home.js
+++ src/data/content/home.js
@@ -1,5 +1,8 @@
+import profileImage from "../images/me.png";
+
 export const HOME={
     name:"Dilip Agheda",
+    image:profileImage,
     jobTitles:["Full Stack Web Developer","Test Automation Engineer","Tea Addict!"],
     tagLine:"Let's build something together!"
 }
