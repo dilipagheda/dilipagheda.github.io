2c7459933f8c04ea894c7dee890a4a7e0bddac51
Dilip <dilip_agheda@yahoo.com>
2019-05-06 09:23:26 +1000

    Designed initial data structure for the project portfolio.

---
+++ src/data/content/portfolio.js
@@ -0,0 +1,34 @@
+// Projects from Frontend Techdegree
+const FETD = [
+    {
+        title: "",
+        coverImage: "",
+        gitHubLink:"",
+        liveView:"",
+        moreInfo: {
+            description:[{
+                heading:"",
+                p:["","",""]
+            }],
+            myLearnings:["","",""],
+            images:[]
+        }
+    }
+]
+
+// Projects from Fullstack JavaScript Techdegree
+const FSJSTD = [
+
+]
+
+// Projects from Android Nanodegree Term1
+const ANDRND = [
+
+]
+
+
+export const PORTFOLIO={
+    fetd:FETD,
+    fsjstd:FSJSTD,
+    andrnd:ANDRND
+}
