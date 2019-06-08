609dc27ec0deb2e589aca8c7e4039352be8f841d
Dilip <dilip_agheda@yahoo.com>
2019-06-08 15:56:19 +1000

    Added content and pdf file

--- src/data/content/adventures.js
+++ src/data/content/adventures.js
@@ -1,8 +1,8 @@
 export const ADVENTURES= {
     p:["As part of my coding adventures, I built variety of projects across front-end to back-end which utilises technologies like HTML,CSS,JavaScript, jQuery, Object Oriented Programming, React, Redux, Node.js, Express, MondoDB and more!." +
         "These projects got reviewed by mentors who provided some great feedback.",
-        "I also got my hands dirty with some Android Projects to get further exposure to Mobile development too.",
-        "I am currently building more side projects of my own and also helping out an organisation like Code.Sydney as a Voluntary Contributer to their Web project."
+        "I also got my hands dirty with mobile development using Flutter, React Native and Native Android Projects. After using Flutter though, I feel that building mobile apps can't get easier and quicker. I tested Flutter apps on physical devices and it works like a charm for both iOS and Android.",
+        "I am currently building more side projects of my own and also helping out an organisation like Code.Sydney as a Voluntary Contributer to their Web project. I also take up part-time Freelance work for Clients who needs to build a quick prototype to see their idea in action or need to do enhancement to their current production system."
     ],
     certificates:
         [{
