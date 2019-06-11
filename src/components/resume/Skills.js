cfdb1d3149f767feed5488a66a1c71415e6599b8
Dilip <dilip_agheda@yahoo.com>
2019-06-11 21:34:38 +1000

    Refactored renderKeywords method to a seperate component

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -1,36 +1,27 @@
 import React, {Component} from 'react';
-
-
-
-
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
+import Keywords from "../common/Keywords";
 
 class Skills extends Component {
-    renderKeywords = (words) => {
-        return (
-            <div>
-
-                {words.map((item)=><span className="badge badge-pill badge-primary pl-2 pr-2 pt-1 pb-2 mr-1">{item}</span>)}
-            </div>
-        );
-    }
 
     renderSectionListItems = (items)=>{
         return items.map((item)=>(
             <li className="list-group-item">
-                <span className="skill-level badge badge-info">{item.level}</span>
-                <div className="d-flex flex-row">
 
+                <div className="d-flex flex-row">
                     <div className="d-flex flex-column justify-content-center pl-2 pr-2 mr-3" style={{width:"50px"}}>
                         <FontAwesomeIcon icon={item.icon} size="2x" color="black" className="text-center"/>
                     </div>
-
                     <div className="d-flex flex-column justify-content-between">
-                            <span className="item-heading mr-auto">{item.heading}</span>
-
-                        {this.renderKeywords(item.tags)}
+                        <div className="d-flex flex-row">
+                            <span className="item-heading">{item.heading}</span>
+                            <div>
+                                <span className="skill-level badge badge-info">{item.level}</span>
+                            </div>
+                        </div>
+                        <Keywords words={item.tags}/>
                     </div>
                 </div>
             </li>
@@ -47,10 +38,11 @@ class Skills extends Component {
                         </div>
                         <div className="col-md-8">
                             <div className="card">
-                                <ul className="list-group list-group-flush">
-
-                                    {this.renderSectionListItems(item.items)}
-                                </ul>
+                                <div className="card-body">
+                                    <ul className="list-group list-group-flush">
+                                        {this.renderSectionListItems(item.items)}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
