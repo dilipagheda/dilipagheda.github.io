43b8539296c172137a6fb44e8586431d5c7783fd
Dilip <dilip_agheda@yahoo.com>
2019-06-14 20:52:06 +1000

    Added Divider to Skills component

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -3,13 +3,13 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import Keywords from "../common/Keywords";
+import Divider from "../common/Divider";
 
 class Skills extends Component {
 
     renderSectionListItems = (items)=>{
         return items.map((item)=>(
             <li className="list-group-item">
-
                 <div className="d-flex flex-row">
                     <div className="d-flex flex-column justify-content-center pl-2 pr-2 mr-3" style={{width:"50px"}}>
                         <FontAwesomeIcon icon={item.icon} size="2x" color="black" className="text-center"/>
@@ -30,23 +30,26 @@ class Skills extends Component {
 
     renderSections = ()=>{
         return(
-            this.props.skills.map((item)=>{
+            this.props.skills.map((item, index)=>{
                 return (
-                    <div className="row p-2">
-                        <div className="col-md-4">
-                            <h4>{item.heading}</h4>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card">
-                                <div className="card-body">
-                                    <ul className="list-group list-group-flush">
-                                        {this.renderSectionListItems(item.items)}
-                                    </ul>
+                    <>
+                        <div className="row p-2">
+                            <div className="col-md-4">
+                                <h4>{item.heading}</h4>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <ul className="list-group list-group-flush">
+                                            {this.renderSectionListItems(item.items)}
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                );
+                        <Divider shouldDisplay={this.props.skills.length-1!==index}/>
+                    </>
+            );
             }
     )
 
