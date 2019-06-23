7e51e2baf22fb579d57af4a867232be8f43a4f3e
Dilip <dilip_agheda@yahoo.com>
2019-06-23 22:30:46 +1000

    Added data and component for Education

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,6 +5,7 @@ import {PORTFOLIO} from "../data/content/portfolio";
 import {RESUME} from "../data/content/resume/resume";
 import {EXPERIENCE} from '../data/content/experience';
 import {CERTIFICATES} from "../data/content/education/certificates";
+import {EDUCATION} from "../data/content/education/education";
 
 export const initialState = {
     aboutme: ABOUTME,
@@ -13,7 +14,8 @@ export const initialState = {
     portfolio: PORTFOLIO,
     resume:RESUME,
     experience:EXPERIENCE,
-    certificates:CERTIFICATES
+    certificates:CERTIFICATES,
+    education:EDUCATION
 };
 
 export const Reducer = (state = initialState, action) => {
