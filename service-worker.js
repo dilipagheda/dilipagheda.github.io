2f04e32b5243447e93ee79ec376271e438a7e8c2
Dilip Agheda <dilip.agheda@origin.com.au>
2025-07-11 08:21:12 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d094ee578889ead6ede4fa5ea489429.js"
+  "/precache-manifest.9aaa2e738595c9727fd0cde49aee14d5.js"
 );
 
 self.addEventListener('message', (event) => {
