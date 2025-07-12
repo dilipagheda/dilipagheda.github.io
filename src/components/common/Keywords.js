3cf161469c5829a32d73e02aea7193348fe24327
Dilip Agheda <dilip.agheda@origin.com.au>
2025-07-12 14:46:42 +1000

    Refactor: improve code quality, address linter warnings, and enhance documentation

--- src/components/common/Keywords.js
+++ src/components/common/Keywords.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Keywords = (props) =>{
         return (
             <div className="d-flex flex-row flex-wrap">
-                {props.words.map((item)=><span className="keyword pl-2 pr-2 pt-1 pb-2 mr-1 mb-1">{item}</span>)}
+                {props.words && props.words.map((item, index)=><span key={index} className="keyword pl-2 pr-2 pt-1 pb-2 mr-1 mb-1">{item}</span>)}
             </div>
           );
 }
